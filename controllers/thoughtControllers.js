effabcf82c72aa4aedb98f06602cb98150b8c4c1
Fernando Almeida <nando.lalmeida@gmail.com>
2022-07-10 12:27:39 -0400

    added initial thoughts logic

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -4,6 +4,10 @@ const { Thought } = require('../models');
 const thoughtController = {
     getAllThoughts(req, res) {
         Thought.find({})
+            .populate({
+                path: "reactions",
+                select: "-__v",
+            })
             .select("-__v")
             .sort({ _id: -1 })
             .then((response) => res.json(response))
@@ -15,12 +19,19 @@ const thoughtController = {
 
     getSingleThought({ params }, res) {
         Thought.findOne({ _id: params.id })
-            // .populate({
-            //     path: "thoughts",
-            //     select: "-__v",
-            // })
+            .populate({
+                path: "reactions",
+                select: "-__v",
+            })
             .select("-__v")
-            .then((response) => res.json(response))
+            .sort({ _id: -1 })
+            .then((response) => {
+                if (!response) {
+                    res.status(404).json({ message: "No thought found for this id"});
+                    return;
+                }
+                res.json(response);
+            })
             .catch((err) => {
                 console.log(err);
                 res.sendStatus(400);
